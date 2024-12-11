0e5e2a60aef184651c3188e021de47626fab9e9d
Jadussy <jade.craft83@gmail.com>
2024-12-11 17:03:15 +0100

    testt

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 // Recupe le <p> pour écrire la valeur du bouton cliquer actuel
-const pNode = document.getElementById("ecrit")
+const pNode = document.getElementById("ecritJ")
 
 // Ajouts des variables des boutons
 const Pierre = document.getElementById("Pierre");
 const Feuille = document.getElementById("Feuille");
 const Ciseaux = document.getElementById("Ciseaux");
-//zdzezefz
+
+var tour = 0;
+var stop = prompt("Stop pour arreter")
+var victoire = 0;
+
+// Les boutons P F C
+while stop != "stop" {
+    tour += 1;
+     
 
 Pierre.addEventListener("click", (event) => {
     pNode.innerText = "Pierre";
@@ -20,5 +28,10 @@ Ciseaux.addEventListener("click", (event) => {
     pNode.innerText =  "Ciseaux";
     temp.push("C");
 });
+
+
+}
+alert("tour 1");
+// random
 
 
