fa45b6b5f47335541f98eabfa12e5a1961a3e7a4
Jadussy <jade.craft83@gmail.com>
2024-12-11 17:22:12 +0100

    pause de 17h

--- script.js
+++ script.js
@@ -1,37 +1,49 @@
 // Recupe le <p> pour écrire la valeur du bouton cliquer actuel
-const pNode = document.getElementById("ecritJ")
+const pNodeJ = document.getElementById("ecritJ")
+const pNodeO = document.getElementById("ecritO")
 
 // Ajouts des variables des boutons
 const Pierre = document.getElementById("Pierre");
 const Feuille = document.getElementById("Feuille");
 const Ciseaux = document.getElementById("Ciseaux");
 
-var tour = 0;
-var stop = prompt("Stop pour arreter")
+var tour = 1;
+var stop;
 var victoire = 0;
 
-// Les boutons P F C
-while stop != "stop" {
-    tour += 1;
-     
+// Fonction qui permet de recuperer le choix aléatoire de l'ordi entre les P,F et C
+function OrdiChoixRandom() {
+    var choix = Math.floor(Math.random() * 3);
+    if (choix === 0 ) {
+        return "Pierre"
+    }
+    else if (choix === 1) {
+        return "Feuille"
+    }
+    else if (choix === 2) {
+        return "Ciseaux"
+    }
+}
+
+// Ecriture du choix de l'ordi dans la page
+
+
+
+alert("tour "+tour);
+tour = tour + 1;
 
 Pierre.addEventListener("click", (event) => {
-    pNode.innerText = "Pierre";
-    temp.push("P");
+        pNodeJ.innerText = "Pierre";
+        pNodeO.innerText = OrdiChoixRandom();
 });
-
 Feuille.addEventListener("click", (event) => {
-    pNode.innerText =  "Feuille";
-    temp.push("F");
+        pNodeJ.innerText =  "Feuille";
+        pNodeO.innerText = OrdiChoixRandom();
 });
 Ciseaux.addEventListener("click", (event) => {
-    pNode.innerText =  "Ciseaux";
-    temp.push("C");
+        pNodeJ.innerText =  "Ciseaux";
+        pNodeO.innerText = OrdiChoixRandom();
 });
 
-
-}
-alert("tour 1");
-// random
 
 
